a5959a4ccce2d6e6f3ce5b8309a5723ec6fd302a
Molly <mojeanmac@gmail.com>
2022-01-28 10:32:58 -0800

    breaking it on purpose :)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -21,12 +21,6 @@ public class MarkdownParse {
             if (openParen == -1 && closeParen == -1){
                 break;
             }
-            if ((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
-            nextCloseBracket + 1 != openParen ||
-            markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1) {
-                currentIndex = closeParen + 1;
-                continue;
-	        }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
