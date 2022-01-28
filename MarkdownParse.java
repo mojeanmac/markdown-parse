359faa897b9dca341a384ae4dad8f6f6fdcb6145
Molly <mojeanmac@gmail.com>
2022-01-28 11:55:06 -0800

    yuh

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -21,7 +21,9 @@ public class MarkdownParse {
             if (openParen == -1 || closeParen == -1 || nextCloseBracket == -1){
                 break;
             }
-            if ((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!')){
+            if ((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
+            nextCloseBracket + 1 != openParen ||
+            markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1){
                 currentIndex = closeParen + 1;
                 continue;
                }
