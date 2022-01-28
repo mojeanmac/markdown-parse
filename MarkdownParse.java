eaf040b3193d90d3b36139576beb046c92aa4d3b
Molly <mojeanmac@gmail.com>
2022-01-28 10:59:55 -0800

    trying this again unfortunately

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -21,6 +21,10 @@ public class MarkdownParse {
             if (openParen == -1 || closeParen == -1 || nextCloseBracket == -1){
                 break;
             }
+            if ((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!')){
+                currentIndex = closeParen + 1;
+                continue;
+               }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
